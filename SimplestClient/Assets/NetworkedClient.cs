57a07490d903ffcb131b1c2bc4cd40ac654f98f5
Fernando <linksgreenhat@gmail.com>
2021-08-26 13:37:53 -0400

    clean up complete

---
+++ SimplestClient/Assets/NetworkedClient.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkedClient : MonoBehaviour
+{
+
+    int connectionID;
+    int maxConnections = 1000;
+    int reliableChannelID;
+    int unreliableChannelID;
+    int hostID;
+    int socketPort = 5491;
+    byte error;
+    bool isConnected = false;
+    int ourClientID;
+
+    const bool connectToFernando = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+            SendMessageToHost("Hello from client");
+
+        UpdateNetworkConnection();
+    }
+
+    private void UpdateNetworkConnection()
+    {
+        if (isConnected)
+        {
+            int recHostID;
+            int recConnectionID;
+            int recChannelID;
+            byte[] recBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostID, out recConnectionID, out recChannelID, recBuffer, bufferSize, out dataSize, out error);
+
+            switch (recNetworkEvent)
+            {
+                case NetworkEventType.ConnectEvent:
+                    Debug.Log("connected.  " + recConnectionID);
+                    ourClientID = recConnectionID;
+                    break;
+                case NetworkEventType.DataEvent:
+                    string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                    ProcessRecievedMsg(msg, recConnectionID);
+                    //Debug.Log("got msg = " + msg);
+                    break;
+                case NetworkEventType.DisconnectEvent:
+                    isConnected = false;
+                    Debug.Log("disconnected.  " + recConnectionID);
+                    break;
+            }
+        }
+    }
+    
+    private void Connect()
+    {
+
+        if (!isConnected)
+        {
+            Debug.Log("Attempting to create connection");
+
+            NetworkTransport.Init();
+
+            ConnectionConfig config = new ConnectionConfig();
+            reliableChannelID = config.AddChannel(QosType.Reliable);
+            unreliableChannelID = config.AddChannel(QosType.Unreliable);
+            HostTopology topology = new HostTopology(config, maxConnections);
+            hostID = NetworkTransport.AddHost(topology, 0);
+            Debug.Log("Socket open.  Host ID = " + hostID);
+
+
+            if(connectToFernando)
+                connectionID = NetworkTransport.Connect(hostID, "142.112.20.199", socketPort, 0, out error);  //  server is not local
+            else
+                connectionID = NetworkTransport.Connect(hostID, "192.168.2.22", socketPort, 0, out error); // server is local on network
+
+            if (error == 0)
+            {
+                isConnected = true;
+
+                Debug.Log("Connected, id = " + connectionID);
+
+            }
+        }
+    }
+    
+    public void Disconnect()
+    {
+        NetworkTransport.Disconnect(hostID, connectionID, out error);
+    }
+    
+    public void SendMessageToHost(string msg)
+    {
+        byte[] buffer = Encoding.Unicode.GetBytes(msg);
+        NetworkTransport.Send(hostID, connectionID, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
+    }
+
+    private void ProcessRecievedMsg(string msg, int id)
+    {
+        Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+    }
+
+    public bool IsConnected()
+    {
+        return isConnected;
+    }
+
+
+}
